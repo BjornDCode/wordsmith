83b45298d57af55c9504e4428fed612c3111221d
Bjørn Lindholm <14328774+BjornDCode@users.noreply.github.com>
2025-01-14 13:44:24 +0100

    Render window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use gpui::{div, prelude::*, rgb, AppContext, WindowOptions};
+
 fn main() {
-    println!("Hello, world!");
+    gpui::App::new().run(|context: &mut AppContext| {
+        context
+            .open_window(WindowOptions::default(), |context| {
+                context.new_view(|_cx| Wordsmith::new())
+            })
+            .unwrap();
+    });
+}
+
+struct Wordsmith {}
+
+impl Wordsmith {
+    pub fn new() -> Wordsmith {
+        Wordsmith {}
+    }
+}
+
+impl Render for Wordsmith {
+    fn render(&mut self, _context: &mut gpui::ViewContext<Self>) -> impl gpui::IntoElement {
+        div().text_color(rgb(0xffffff)).child("Yo")
+    }
 }
