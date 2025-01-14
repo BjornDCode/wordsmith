cb0d3873970f3b9d40e46cd529df63c564b55dcd
Bjørn Lindholm <14328774+BjornDCode@users.noreply.github.com>
2025-01-14 16:50:00 +0100

    Toggle sidebar

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use gpui::{
-    actions, div, prelude::*, px, rems, rgb, size, AppContext, Bounds, KeyBinding, WindowBounds,
-    WindowOptions,
+    actions, div, prelude::*, px, rems, rgb, size, AppContext, Bounds, FocusHandle, FocusableView,
+    KeyBinding, ViewContext, WindowBounds, WindowOptions,
 };
 
 const COLOR_WHITE: u32 = 0xffffff;
@@ -19,13 +19,16 @@ const COLOR_GRAY_800: u32 = 0x1f2937;
 const COLOR_GRAY_900: u32 = 0x111827;
 const COLOR_GRAY_950: u32 = 0x030712;
 
-actions!(app, [Quit]);
+actions!(app, [Quit, ToggleSidebar]);
 
 fn main() {
     gpui::App::new().run(|context: &mut AppContext| {
         let bounds = Bounds::centered(None, size(px(800.), px(600.)), context);
 
-        context.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
+        context.bind_keys([
+            KeyBinding::new("cmd-q", Quit, None),
+            KeyBinding::new("cmd-b", ToggleSidebar, None),
+        ]);
 
         context.on_action(|_: &Quit, context| context.quit());
 
@@ -35,7 +38,7 @@ fn main() {
                     window_bounds: Some(WindowBounds::Windowed(bounds)),
                     ..Default::default()
                 },
-                |context| context.new_view(|_cx| Wordsmith::new()),
+                |context| context.new_view(|context| Wordsmith::new(context.focus_handle())),
             )
             .unwrap();
 
@@ -49,29 +52,56 @@ fn main() {
 
         window
             .update(context, |view, context| {
+                context.focus_self();
                 context.activate(true);
             })
             .unwrap();
     });
 }
 
-struct Wordsmith {}
+struct Wordsmith {
+    focus_handle: FocusHandle,
+    show_sidebar: bool,
+}
 
 impl Wordsmith {
-    pub fn new() -> Wordsmith {
-        Wordsmith {}
+    pub fn new(focus_handle: FocusHandle) -> Wordsmith {
+        Wordsmith {
+            focus_handle,
+            show_sidebar: true,
+        }
+    }
+
+    fn toggle_sidebar(&mut self, _: &ToggleSidebar, context: &mut ViewContext<Self>) {
+        self.show_sidebar = !self.show_sidebar;
+
+        context.notify();
+    }
+}
+
+impl FocusableView for Wordsmith {
+    fn focus_handle(&self, _context: &AppContext) -> FocusHandle {
+        self.focus_handle.clone()
     }
 }
 
 impl Render for Wordsmith {
     fn render(&mut self, context: &mut gpui::ViewContext<Self>) -> impl gpui::IntoElement {
+        let children = if self.show_sidebar {
+            vec![main_content(), sidebar()]
+        } else {
+            vec![main_content()]
+        };
+
         div()
             .flex()
             .flex_row()
+            .track_focus(&self.focus_handle(context))
+            .on_action(context.listener(Self::toggle_sidebar))
             .bg(rgb(COLOR_WHITE))
             .size_full()
             .text_color(rgb(COLOR_BLACK))
-            .children(vec![main_content(), sidebar()])
+            .children(children)
     }
 }
 
