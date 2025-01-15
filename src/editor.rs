fdc8ee313182e06d641d23b35e8d9d0b728ec08e
Bjørn Lindholm <14328774+BjornDCode@users.noreply.github.com>
2025-01-15 16:43:20 +0100

    Render dummy cursor when focused

--- src/editor.rs
+++ src/editor.rs
@@ -31,16 +31,20 @@ impl FocusableView for Editor {
 
 impl Render for Editor {
     fn render(&mut self, context: &mut gpui::ViewContext<Self>) -> impl gpui::IntoElement {
+        let is_focused = self.focus_handle.is_focused(context);
+
         div()
             .track_focus(&self.focus_handle(context))
             .key_context("editor")
             .on_action(context.listener(Self::temp))
             .pt_8()
+            .group("editor-container")
             .child(
                 div()
                     .bg(rgb(COLOR_PINK))
                     .w(px(480.))
-                    .child(self.content.clone()),
+                    .child(self.content.clone())
+                    .when(is_focused, |this| this.child("CURSOR")),
             )
     }
 }
